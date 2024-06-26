cdbdb6ef86953d3b1877a0ac15846670d7dc79ac
Philip Zimmermann <philip.zimmermann@tum.de!>
2024-06-26 18:54:02 +0200

    added gtest

---
+++ test/loader_test.cpp
@@ -0,0 +1,13 @@
+#include <gtest/gtest.h>
+
+namespace
+{
+    // Demonstrate some basic assertions.
+    TEST(LoaderTest, BasicAssertions)
+    {
+        // Expect two strings not to be equal.
+        EXPECT_STRNE("hello", "world");
+        // Expect equality.
+        EXPECT_EQ(7 * 6, 42);
+    }
+}
