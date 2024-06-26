d9ab74d9a32b290322adf7f23df94c849a2a4f73
Philip Zimmermann <philip.zimmermann@tum.de!>
2024-06-26 18:25:41 +0200

    add Cmake and first files

---
+++ src/loader.cpp
@@ -0,0 +1,7 @@
+#include "loader.h"
+#include <iostream>
+
+void load_raw()
+{
+    std::cout << "Loading RAW" << std::endl;
+}
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "loader.h"
+
+int main(int, char **)
+{
+    printf("Hello, from raw_editor!\n");
+    load_raw();
+}
