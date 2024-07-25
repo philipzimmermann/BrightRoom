7435c668a936867d7b562f92f4b8e827acd03830
Philip Zimmermann <philip.zimmermann@tum.de!>
2024-07-26 00:29:52 +0200

    fixed CMakeLists

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,13 @@
 #include <stdio.h>
+#include <QApplication>
 #include "loader.h"
+#include "main_window.h"
 
-int main(int, char **)
-{
-    printf("Hello, from raw_editor!\n");
-    load_raw();
+int main(int argc, char* argv[]) {
+  printf("Hello, from raw_editor!\n");
+  QApplication app(argc, argv);
+  QGuiApplication::setApplicationDisplayName(MainWindow::tr("Image Viewer"));
+  MainWindow main_window;
+  main_window.show();
+  return app.exec();
 }
